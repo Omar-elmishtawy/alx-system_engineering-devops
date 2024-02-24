5c8532a51beb1b4a8554c062cdad93bde46592ee
root <omar.elmishtawy@gmail.com>
2024-02-23 18:30:56 -0800

    dsa

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# install python package
+# Install flask (2.1.0)
+exec {'install-flask':
+require => Exec['install-python'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
 
-exec { 'install python package':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+exec {'install-python':
+command => '/usr/bin/which python3'
 }
